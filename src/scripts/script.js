e71e8ffde10855545441f02ac23632c66351bd9f
dzdanowicz <contact@dzdanowicz.com>
2022-01-17 03:39:49 +0100

    Hero section in progress

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,8 +2,14 @@ const searchTabs = document.querySelectorAll(".hero__search-tab");
 
 searchTabs.forEach((item) => {
   item.addEventListener("click", () => {
-    let selectedItem = document.querySelector(".selected");
-    selectedItem.classList.remove("selected");
+    document.querySelector(".selected").classList.remove("selected");
     item.classList.add("selected");
+    if (searchTabs[0].classList.contains("selected")) {
+      console.log("radius");
+      document.querySelector(".hero__search-bar").style.borderRadius =
+        "0 20px 20px 20px";
+    } else {
+      document.querySelector(".hero__search-bar").style.borderRadius = "20px";
+    }
   });
 });
