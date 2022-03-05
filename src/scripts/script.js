1d8ff6d339ab913c46eab8f26093d549d4e93314
dzdanowicz <contact@dzdanowicz.com>
2022-03-05 19:20:15 +0100

    Menu in header now supports mobile devicesa and added hamburger icon with its functionality

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,11 +5,28 @@ searchTabs.forEach((item) => {
     document.querySelector(".selected").classList.remove("selected");
     item.classList.add("selected");
     if (searchTabs[0].classList.contains("selected")) {
-      console.log("radius");
       document.querySelector(".hero__search-bar").style.borderRadius =
         "0 20px 20px 20px";
     } else {
       document.querySelector(".hero__search-bar").style.borderRadius = "20px";
     }
   });
+});
+
+let navMenuItems = document.querySelector(".nav__menu-items");
+let darkLayer = document.querySelector(".dark-layer");
+document.querySelector(".nav__side-menu-btn").addEventListener("click", () => {
+  navMenuItems.classList.toggle("side-menu-active");
+  darkLayer.classList.toggle("dark-layer-active");
+});
+
+window.addEventListener("click", (elem) => {
+  console.log(elem);
+  if (
+    navMenuItems.classList.contains("side-menu-active") &&
+    !elem.target.classList.contains("fa-solid")
+  ) {
+    navMenuItems.classList.remove("side-menu-active");
+    darkLayer.classList.remove("dark-layer-active");
+  }
 });
